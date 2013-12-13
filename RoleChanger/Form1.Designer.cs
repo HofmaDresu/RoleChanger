10ead0a333036a9162ce8439fc41168373a67f3d
Matt Hoffman <hofmadresu@gmail.com>
2013-12-13 07:54:21 -0500

    Initial Commit

---
+++ RoleChanger/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace RoleChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.UserList = new System.Windows.Forms.ComboBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.RolesBox = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.UserList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.RolesBox);
+            this.splitContainer1.Size = new System.Drawing.Size(633, 651);
+            this.splitContainer1.SplitterDistance = 211;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // UserList
+            // 
+            this.UserList.FormattingEnabled = true;
+            this.UserList.Location = new System.Drawing.Point(12, 47);
+            this.UserList.Name = "UserList";
+            this.UserList.Size = new System.Drawing.Size(195, 21);
+            this.UserList.TabIndex = 0;
+            this.UserList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(546, 657);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(12, 657);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            // 
+            // RolesBox
+            // 
+            this.RolesBox.FormattingEnabled = true;
+            this.RolesBox.Location = new System.Drawing.Point(32, 47);
+            this.RolesBox.Name = "RolesBox";
+            this.RolesBox.Size = new System.Drawing.Size(363, 589);
+            this.RolesBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Roles";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 690);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox UserList;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.CheckedListBox RolesBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RoleChanger/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoleChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupData();
+        }
+
+        private void SetupData()
+        {
+            PopulateRoleList();
+            PopulateUserList();
+        }
+
+        private void PopulateUserList()
+        {
+            var userList = new List<string>();
+            var con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TargetDatabase"].ConnectionString);
+            con.Open();
+            var userReader = new SqlCommand("SELECT name FROM sys.database_principals where type in ('U', 'S')", con).ExecuteReader();
+
+            while (userReader.Read())
+            {
+                userList.Add(userReader["name"].ToString());
+            }
+            con.Close();
+
+            UserList.DataSource = userList;
+        }
+
+        private void PopulateRoleList()
+        {
+            var roleList = new List<string>();
+            var con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TargetDatabase"].ConnectionString);
+            con.Open();
+            var roleReader = new SqlCommand("SELECT name FROM sys.database_principals where type in ('R')", con).ExecuteReader();
+
+            while (roleReader.Read())
+            {
+                roleList.Add(roleReader["name"].ToString());
+            }
+            con.Close();
+
+            RolesBox.DataSource = roleList;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (var i = 0; i < RolesBox.Items.Count; i++)
+            {
+                RolesBox.SetItemChecked(i, false);
+            }
+
+            var userName = UserList.Items[((ComboBox)sender).SelectedIndex];
+
+            var con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TargetDatabase"].ConnectionString);
+            con.Open();
+            //TODO: Fix to use parameters
+            var roleReader = new SqlCommand(string.Format("EXEC sp_helpuser '{0}'", userName), con).ExecuteReader();
+
+            while (roleReader.Read())
+            {
+                for (var i = 0; i < RolesBox.Items.Count; i++ )
+                {
+                    if (RolesBox.Items[i].ToString() == roleReader["RoleName"].ToString()) RolesBox.SetItemChecked(i, true);
+                }
+            }
+
+            con.Close();
+        }
+    }
+}
